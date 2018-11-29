18908eff2519175894df91788b8207d5fb180f32
Celine Huang <celinehuang@Celines-MacBook-Pro.local>
2018-11-29 15:38:18 -0500

    Fixed bugs in part 5

--- sr_container_helpers.c
+++ sr_container_helpers.c
@@ -168,7 +168,7 @@ int setup_syscall_filters(){
 
     // syscall for chmod
     filter_set_status = seccomp_rule_add(seccomp_ctx, SCMP_FAIL, SCMP_SYS(chmod), 2, 
-                            SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID), SCMP_A0(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID));
+                            SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID), SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID));
     if (filter_set_status) {
         if (seccomp_ctx)
             seccomp_release(seccomp_ctx);
